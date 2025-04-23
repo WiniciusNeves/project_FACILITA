442ee466e5ddbf23d69838018553d39ace37d9fc
Luiz Henrique <luizhsgassis.dev@gmail.com>
2025-04-23 10:43:29 -0300

    feat(components): update GoogleButton to use an image instead of an icon

--- Frontend/src/components/GoogleButton.tsx
+++ Frontend/src/components/GoogleButton.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Image, Text, TouchableOpacity } from "react-native";
 import { googleButton } from "../styles/login";
-import Icon from "react-native-vector-icons/FontAwesome";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function GoogleButton() {
   return (
-    <TouchableOpacity style={googleButton.base}>
-      <Icon name="google" size={20} color="#fff" style={{ marginRight: 8 }} />
-      <Text style={googleButton.text}>Entrar pela conta do google</Text> 
+    <TouchableOpacity
+      onPress={() => {}}
+      style={googleButton.base}
+    >
+      <View style={googleButton.content}>
+          <Image 
+              source={require("../assets/img/googleIcon.png")} 
+              style={googleButton.iconImage}
+          />
+          <View style={googleButton.verticalLine} />
+          <Text style={googleButton.text}>Entrar pela conta do google</Text>
+      </View>
     </TouchableOpacity>
   );
 }
